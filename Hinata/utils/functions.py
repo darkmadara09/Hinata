f86d28f8dbdfcbe3e321bcfce91a21c3f74f600a
PriyanshGoswami <106391059+PriyanshGoswami@users.noreply.github.com>
2022-05-28 01:32:18 +0530

    Update functions.py

--- Hinata/utils/functions.py
+++ Hinata/utils/functions.py
@@ -32,9 +32,9 @@ import speedtest
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
 from pyrogram.types import Message
 
-from Zaid import aiohttpsession as aiosession
-from Zaid.ex_plugins.dbfunctions import start_restart_stage
-from Zaid.utils.http import get, post
+from Hinata import aiohttpsession as aiosession
+from Hinata.ex_plugins.dbfunctions import start_restart_stage
+from Hinata.utils.http import get, post
 
 
 async def restart(m: Message):
