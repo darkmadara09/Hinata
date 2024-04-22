9522e122fa4e6290bf3f1e97b7a0992611c2fbc3
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-04-22 13:28:43 +0530

    Update __init__.py

--- Hinata/__init__.py
+++ Hinata/__init__.py
@@ -133,9 +133,6 @@ if ENV:
     except ValueError:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
-else:
-    from Hinata.config import Development as Config
-
     TOKEN = Config.TOKEN
 
     try:
